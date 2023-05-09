0b5be2edef643aa7ee13c093a7b2103d6ed63b68
q0s <37809416+babinkos@users.noreply.github.com>
2023-05-09 21:40:44 +0300

    ecs/module/main.tf : added missing https sg for alb

--- terraform/ecs/module/main.tf
+++ terraform/ecs/module/main.tf
@@ -177,7 +177,10 @@ module "alb_sg" {
   description = "Service security group"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_rules       = ["http-80-tcp"]
+  ingress_rules = [
+    "http-80-tcp",
+    "https-443-tcp"
+  ]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
